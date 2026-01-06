8b66df6d78c05ace4688586a7b34ba9df38628e5
odajima-suzuna <k025g0091@g.neec.ac.jp>
2026-01-06 14:54:44 +0900

    5-2できた

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -7,26 +7,26 @@ GameScene::GameScene() {}
 
 GameScene::~GameScene() {
 
-	// スプライトの解放
-	delete sprite_;
-
 	// 3Dモデルの解放
 	delete model_;
 
+#ifdef _DEBUG
+
 	// デバッグ用カメラの解放
 	delete debugCamera_;
+
+#endif // _DEBUG
+
+	// ブロック用ワールド行列の解放
+	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlockLine) {
+			delete worldTransformBlock;
+		}
+	}
+	worldTransformBlocks_.clear();
 }
 
 void GameScene::Initialize() {
-
-	// 画像読み込み
-	grHandleSushi_ = TextureManager::Load("./Resources/Images/sushi.png");
-
-	// SE読み込み
-	seHandleMokugyo = Audio::GetInstance()->LoadWave("./Resources/mokugyo.wav");
-
-	// スプライト生成
-	sprite_ = Sprite::Create(grHandleSushi_, {50.0f, 50.0f});
 
 	// 3Dモデル生成
 	model_ = Model::Create();
@@ -37,73 +37,162 @@ void GameScene::Initialize() {
 	// カメラ初期化
 	camera_.Initialize();
 
-	// ライン描画が参照するカメラを指定
-	PrimitiveDrawer::GetInstance()->SetCamera(&camera_);
+#ifdef _DEBUG
 
 	// デバッグ用カメラ生成
 	debugCamera_ = new DebugCamera(1280, 720);
 
-	// 軸方向の表示を有効化
-	AxisIndicator::GetInstance()->SetVisible(true);
+#endif // _DEBUG
 
-	// 軸方向表示が参照するビュープロジェクションを指定
-	AxisIndicator::GetInstance()->SetTargetCamera(&debugCamera_->GetCamera());
+	// 要素数
+	const uint32_t kNumBlockVirtical = 10;
+	const uint32_t kNumBlockHorizontal = 20;
+
+	// ブロック1個の横幅
+	const float kBlockWidth = 2.0f;
+	const float kBlockHeight = 2.0f;
+
+	// 配列の要素数を変更
+	// 列数を指定(縦)
+	worldTransformBlocks_.resize(kNumBlockVirtical);
+	for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
+		// 列ごとの要素数を指定(横)
+		worldTransformBlocks_[i].resize(kNumBlockHorizontal);
+	}
+
+	// ブロックの生成
+	for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
+		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
+			worldTransformBlocks_[i][j] = new KamataEngine::WorldTransform();
+			worldTransformBlocks_[i][j]->Initialize();
+			worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j * 2;
+			worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
+		}
+	}
 }
 
 void GameScene::Update() {
 
-	// スプライトの現在座標を取得
-	Vector2 position = sprite_->GetPosition();
+	// ブロックの更新
+	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlockLine) {
+			if (!worldTransformBlock) {
+				continue;
+			}
 
-	// 座標を{ 2,1 }移動
-	position.x += 2.0f;
-	position.y += 1.0f;
+			// アフィン変換行列の作成
+			Matrix4x4 affineMatrix = MakeAffineMatrix(worldTransformBlock->rotation_, worldTransformBlock->scale_, worldTransformBlock->translation_);
 
-	// 移動した結果を反映
-	sprite_->SetPosition(position);
+			worldTransformBlock->matWorld_ = affineMatrix;
 
-	// スペースキーでSE木魚再生
-	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
-		Audio::GetInstance()->PlayWave(seHandleMokugyo, false, 1.0f);
+			worldTransformBlock->TransferMatrix();
+		}
 	}
 
 #ifdef _DEBUG
 
-	// デバッグ用カメラ更新処理
-	debugCamera_->Update();
+	// デバッグ用カメラの起動
+	if (Input::GetInstance()->TriggerKey(DIK_RETURN)) {
+		isDebugCameraActive_ = !isDebugCameraActive_;
+	}
 
-	// ImGui受付開始
-	ImGui::Begin("Debug1");
+	if (isDebugCameraActive_) {
 
-	// デバッグ用テキスト表示
-	ImGui::Text("sushi syouhi kigen %d.%d.%d", 2025, 12, 15);
+		// デバッグ用カメラ更新処理
+		debugCamera_->Update();
 
-	// ImGui受付終了
-	ImGui::End();
+		camera_.matView = debugCamera_->GetCamera().matView;
+		camera_.matProjection = debugCamera_->GetCamera().matProjection;
+
+		camera_.TransferMatrix();
+
+	} else {
+		camera_.UpdateMatrix();
+	}
 
 #endif // DEBUG
 }
 
 void GameScene::Draw() {
 
-	// スプライト描画開始
-	Sprite::PreDraw();
-
-	// スプライト描画
-	// sprite_->Draw();
-
-	// スプライト描画終了
-	Sprite::PostDraw();
-
 	// 3Dモデル描画開始
 	Model::PreDraw();
 
-	// 3Dモデル描画
-	model_->Draw(worldTransform_, debugCamera_->GetCamera(), grHandleSushi_);
+	// ブロックの描画
+	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlockLine) {
+			if (!worldTransformBlock) {
+				continue;
+			}
+
+			model_->Draw(*worldTransformBlock, camera_);
+		}
+	}
 
 	// 3Dモデル描画終了
 	Model::PostDraw();
+}
 
-	// ラインを描画
-	PrimitiveDrawer::GetInstance()->DrawLine3d({0.0f, 0.0f, 0.0f}, {0.0f, 10.0f, 0.0f}, {1.0f, 0.0f, 0.0f, 1.0f});
+KamataEngine::Matrix4x4 GameScene::Multiply(KamataEngine::Matrix4x4 a, KamataEngine::Matrix4x4 b) {
+	Matrix4x4 result;
+
+	for (uint32_t i = 0; i < 4; i++) {
+		for (uint32_t j = 0; j < 4; j++) {
+			result.m[i][j] = 0.0f;
+			for (uint32_t k = 0; k < 4; k++) {
+				result.m[i][j] += a.m[i][k] * b.m[k][j];
+			}
+		}
+	}
+
+	return result;
+}
+
+KamataEngine::Matrix4x4 GameScene::MakeAffineMatrix(KamataEngine::Vector3 rotate, KamataEngine::Vector3 scale, KamataEngine::Vector3 translate) {
+
+	// 回転行列の作成
+	Matrix4x4 rotateMatrix;
+	rotateMatrix.m[0][0] = cosf(rotate.y) * cosf(rotate.z);
+	rotateMatrix.m[0][1] = -(cosf(rotate.y))*sinf(rotate.z);
+	rotateMatrix.m[0][2] = sinf(rotate.y);
+	rotateMatrix.m[0][3] = 0.0f;
+
+	rotateMatrix.m[1][0] = cosf(rotate.x) * sinf(rotate.z) + sinf(rotate.x) * sinf(rotate.y) * cosf(rotate.z);
+	rotateMatrix.m[1][1] = cosf(rotate.x) * cosf(rotate.z) - sinf(rotate.x) * sinf(rotate.y) * sinf(rotate.z);
+	rotateMatrix.m[1][2] = -(sinf(rotate.x))*cosf(rotate.y);
+	rotateMatrix.m[1][3] = 0.0f;
+
+	rotateMatrix.m[2][0] = sinf(rotate.x) * sinf(rotate.z) - cosf(rotate.x) * sinf(rotate.y) * cosf(rotate.z);
+	rotateMatrix.m[2][1] = sinf(rotate.x) * cosf(rotate.z) + cosf(rotate.x) * sinf(rotate.y) * sinf(rotate.z);
+	rotateMatrix.m[2][2] = cosf(rotate.x) * cosf(rotate.y);
+	rotateMatrix.m[2][3] = 0.0f;
+
+	rotateMatrix.m[3][0] = 0.0f;
+	rotateMatrix.m[3][1] = 0.0f;
+	rotateMatrix.m[3][2] = 0.0f;
+	rotateMatrix.m[3][3] = 1.0f;
+
+	// アフィン行列の作成
+	Matrix4x4 affineMatrix;
+	affineMatrix.m[0][0] = scale.x * rotateMatrix.m[0][0];
+	affineMatrix.m[0][1] = scale.x * rotateMatrix.m[0][1];
+	affineMatrix.m[0][2] = scale.x * rotateMatrix.m[0][2];
+	affineMatrix.m[0][3] = 0.0f;
+
+	affineMatrix.m[1][0] = scale.y * rotateMatrix.m[1][0];
+	affineMatrix.m[1][1] = scale.y * rotateMatrix.m[1][1];
+	affineMatrix.m[1][2] = scale.y * rotateMatrix.m[1][2];
+	affineMatrix.m[1][3] = 0.0f;
+
+	affineMatrix.m[2][0] = scale.z * rotateMatrix.m[2][0];
+	affineMatrix.m[2][1] = scale.z * rotateMatrix.m[2][1];
+	affineMatrix.m[2][2] = scale.z * rotateMatrix.m[2][2];
+	affineMatrix.m[2][3] = 0.0f;
+
+	affineMatrix.m[3][0] = translate.x;
+	affineMatrix.m[3][1] = translate.y;
+	affineMatrix.m[3][2] = translate.z;
+	affineMatrix.m[3][3] = 1.0f;
+
+	return affineMatrix;
 }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,20 +1,10 @@
 #pragma once
 #include <KamataEngine.h>
+#include <vector>
 
 class GameScene {
 public:  /*public変数*/
-
 private: /*private変数*/
-
-	// 画像ハンドル
-	uint32_t grHandleSushi_ = 0;
-
-	// 音声ハンドル
-	uint32_t seHandleMokugyo = 0;
-
-	// スプライト
-	KamataEngine::Sprite* sprite_ = nullptr;
-
 	// 3Dモデル
 	KamataEngine::Model* model_ = nullptr;
 
@@ -24,11 +14,18 @@ private: /*private変数*/
 	// カメラ
 	KamataEngine::Camera camera_;
 
+	// ブロック用ワールド行列
+	std::vector<std::vector<KamataEngine::WorldTransform*>> worldTransformBlocks_;
+
+#ifdef _DEBUG
+
 	// デバッグ用カメラ
+	bool isDebugCameraActive_ = false;
 	KamataEngine::DebugCamera* debugCamera_ = nullptr;
 
-public:  /*public関数*/
+#endif // _DEBUG
 
+public: /*public関数*/
 	// コンストラクタ
 	GameScene();
 
@@ -45,5 +42,9 @@ public:  /*public関数*/
 	void Draw();
 
 private: /*private関数*/
+	// 行列の掛け算
+	KamataEngine::Matrix4x4 Multiply(KamataEngine::Matrix4x4 a, KamataEngine::Matrix4x4 b);
 
+	// アフィン行列の作成
+	KamataEngine::Matrix4x4 MakeAffineMatrix(KamataEngine::Vector3 rotate, KamataEngine::Vector3 scale, KamataEngine::Vector3 translate);
 };
